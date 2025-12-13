005e3e641cb4d9d06ae4d66387124c937804d079
Gituse1 <pavlikovskiysasa@gmail.com>
2025-12-13 19:25:04 +0200

    Return Security

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -1,18 +1,18 @@
-package com.example.demo.config; // Заміни на твій пакет
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class CorsConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Дозволяємо CORS для всіх ендпоінтів
-                .allowedOrigins("https://iridescent-gecko-ab947c.netlify.app") // <<< ТВОЯ АДРЕСА ФРОНТЕНДУ!
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Дозволені методи
-                .allowedHeaders("*") // Дозволені заголовки
-                .allowCredentials(true); // Дозволяємо куки, якщо вони використовуються
-    }
-}
+//package com.example.demo.config; // Заміни на твій пакет
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class CorsConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**") // Дозволяємо CORS для всіх ендпоінтів
+//                .allowedOrigins("https://iridescent-gecko-ab947c.netlify.app") // <<< ТВОЯ АДРЕСА ФРОНТЕНДУ!
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Дозволені методи
+//                .allowedHeaders("*") // Дозволені заголовки
+//                .allowCredentials(true); // Дозволяємо куки, якщо вони використовуються
+//    }
+//}
